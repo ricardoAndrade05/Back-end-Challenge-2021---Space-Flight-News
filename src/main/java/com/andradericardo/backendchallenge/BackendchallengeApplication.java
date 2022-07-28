a3cbcc1ca0f139d10d615336b8c7dbf56e45953e
Ricardo Andrade <ricardo.andrade05@gmail.com>
2022-07-28 14:18:14 -0300

    Criação do projeto

---
+++ src/main/java/com/andradericardo/backendchallenge/BackendchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.andradericardo.backendchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendchallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelioalves/coursomc/BackendchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelioalves.coursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
